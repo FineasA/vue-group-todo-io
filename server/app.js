cd988b0a8d0f2d284132675844a9cae017350dbd
Fineas Anton <fineas.anton@gmail.com>
2020-07-30 14:48:11 -0500

    forgot path

--- server/app.js
+++ server/app.js
@@ -1,9 +1,10 @@
 const PORT = process.env.PORT || 8080;
 const express = require("express");
-// const serveStatic = require("serve-static");
+const serveStatic = require("serve-static");
+const path = require("path");
 const app = express();
 
-// app.use("/", serveStatic(path.join(__dirname, "../dist")));
+app.use("/", serveStatic(path.join(__dirname, "../dist")));
 
 const server = app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
